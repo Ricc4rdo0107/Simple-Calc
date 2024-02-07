ae29e6be83d4f64489f6b665f84d85f65311930a
Riccardo Zappitelli <63201347+Ricc4rdo0107@users.noreply.github.com>
2024-02-07 12:49:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ class Calcolatrice:#÷
 
     def GUI(self):
         layout = [
-            #[ sg.Multiline(disabled=True, expand_x=True, border_width=0, no_scrollbar=True, font=("Helvetica", 36),key="-output-") ],
             [ sg.Input(expand_x=True, border_width=0, font=("Helvetica", 36),key="-output-") ],
             [ sg.Button("CE", expand_x=True, size=(3,1), key="-cls-"), sg.Button("←", expand_x=True, size=(3,1), key="-delete-") ],
+            [ sg.Button("%", expand_x=True, size=(3,1)), sg.Button(",", expand_x=True, size=(3,1)), sg.Button("(", expand_x=True, size=(3,1)), sg.Button(")", expand_x=True, size=(3,1)) ],
             [ sg.Button("x²", expand_x=True, size=(3,1), key="-sqrtd-"),sg.Button("^", expand_x=True, size=(3,1)),sg.Button("√", expand_x=True, size=(3,1), key="-sqrt-"),sg.Button("÷", expand_x=True, size=(3,1)) ],
             [ sg.Button("1", expand_x=True , size=(3,1)),sg.Button("2", expand_x=True, size=(3,1)),sg.Button("3", expand_x=True, size=(3,1)),sg.Button("x", expand_x=True, size=(3,1)) ],
             [ sg.Button("4", expand_x=True , size=(3,1)),sg.Button("5", expand_x=True, size=(3,1)),sg.Button("6", expand_x=True, size=(3,1)),sg.Button("-", expand_x=True, size=(3,1)) ],
@@ -22,7 +22,7 @@ class Calcolatrice:#÷
         self.window = sg.Window(title="Calcolatrice", layout=layout, size=self.size)
 
         symbols = ["÷", "x", "-", "+"]
-
+        keep_on_top = False
         while True:
             event, values = self.window.read()
 
@@ -31,20 +31,31 @@ class Calcolatrice:#÷
             else:
                 if event not in ( "-cls-", "-eq-"):
                     self.window["-output-"].update(values["-output-"]+event)
+                try:
+                    op = values["-output-"].replace("x", "*").replace("÷", "/").replace("^", "**").replace(",", ".")
+                    if event == "-eq-":
+                        self.window["-output-"].update(eval(op))
 
-                if event == "-eq-":
-                    self.window["-output-"].update(eval(values["-output-"].replace("x", "*").replace("÷", "/").replace("^", "**")))
+                    if event == "-sqrt-":
+                        self.window["-output-"].update(eval(op))
+                        self.window["-output-"].update(math.sqrt(int(values["-output-"])))
 
-                if event == "-sqrt-":
-                    self.window["-output-"].update(eval(values["-output-"].replace("x", "*").replace("÷", "/").replace("^", "**")))
-                    self.window["-output-"].update(math.sqrt(int(values["-output-"])))
+                    if event == "-sqrtd-":
+                        self.window["-output-"].update(eval(op))
+                        self.window["-output-"].update(int(values["-output-"])**2)
 
-                if event == "-sqrtd-":
-                    self.window["-output-"].update(eval(values["-output-"].replace("x", "*").replace("÷", "/").replace("^", "**")))
-                    self.window["-output-"].update(int(values["-output-"])**2)
+                except SyntaxError:
+                    self.window["-output-"].update("Syntax Error")
+
+                except NameError:
+                    self.window["-output-"].update("Syntax Error")
+
+                except Exception as e:
+                    sg.popup_error(e)
+                    print(op)
 
                 if event == "-delete-":
-                    self.window["-output-"].update(values["-output-"][1:])
+                    self.window["-output-"].update(values["-output-"][:-1])
 
                 if event == "-cls-":
                     self.window["-output-"].update("")
